7acc4bdd87214dae6224078e291735f8d7340169
singh-anshuman <anshumansingh137@gmail.com>
2019-03-16 16:08:58 +0530

    Added code for eureka client service implementation

---
+++ src/main/java/com/anshuman/eurekaClient/UserServiceStarter.java
@@ -0,0 +1,12 @@
+package com.anshuman.eurekaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceStarter.class,args);
+    }
+
+}
